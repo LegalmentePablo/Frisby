6b59a1c8af38ed4b6d3e6f19dfeb11259e1a27bd
LegalmentePablo <pbuitragom2@gmail.com>
2020-10-13 08:52:45 -0500

    Img de frisby en HOME

--- pages/home.vue
+++ pages/home.vue
@@ -1,3 +1,6 @@
 <template>
-    <div>HOME</div>
+    <div>
+        <h1>Bienvenidos!</h1>
+         <img src="https://carloscortes.com.co/wp-content/uploads/2019/10/Logo-del-pollo-frisby-1024x448.jpg" alt="Frisby"> 
+    </div>
 </template>
